533bc6124dbbcf3d09a9ca43237517d74ce8960b
ierva <team.ierva@gmail.com>
2016-09-01 17:44:50 +0200

    update-6

--- lirs-views/box-home.php
+++ lirs-views/box-home.php
@@ -5,8 +5,8 @@
 		
 		<div class="container">
 		
-		<div class="col-md-12 box-padding">
-		<div class="col-md-4">
+		<div class="col-md-12 col-sx-12 box-padding">
+		<div class="col-md-4 col-sx-12">
 		<div class="box-padding-product">
 		<div class="row">
   <div class="col-xs-12 col-md-12">
@@ -15,12 +15,14 @@
     </a>
   </div>
 Lorem Ipsum è un testo segnaposto utilizzato nel settore 
+Lorem Ipsum è un testo segnaposto utilizzato nel settore 
+
 <p><a href="#" class="btn btn-primary btn-block">Vedi</a></p>
 </div>
 </div>
 </div>
 <!--./BOX-->	
-		<div class="col-md-4">
+		<div class="col-md-4 col-sx-12">
 		<div class="box-padding-product">
 		<div class="row">
   <div class="col-xs-12 col-md-12">
@@ -29,12 +31,14 @@ Lorem Ipsum è un testo segnaposto utilizzato nel settore
     </a>
   </div>
 Lorem Ipsum è un testo segnaposto utilizzato nel settore 
+Lorem Ipsum è un testo segnaposto utilizzato nel settore 
+
 <p><a href="#" class="btn btn-primary btn-block">Vedi</a></p>
 </div>
 </div>
 </div>
 <!--./BOX-->
-		<div class="col-md-4">
+		<div class="col-md-4 col-sx-12">
 		<div class="box-padding-product">
 		<div class="row">
   <div class="col-xs-12 col-md-12">
@@ -43,6 +47,8 @@ Lorem Ipsum è un testo segnaposto utilizzato nel settore
     </a>
   </div>
 Lorem Ipsum è un testo segnaposto utilizzato nel settore 
+Lorem Ipsum è un testo segnaposto utilizzato nel settore 
+
 <p><a href="#" class="btn btn-primary btn-block">Vedi</a></p>
 </div>
 </div>
